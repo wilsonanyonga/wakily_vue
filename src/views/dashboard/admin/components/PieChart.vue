5544a8212907c75ab7f09e81d49dbf89890b6ce2
Wilson Anyonga <wilsonanyonga@gmail.com>
2021-01-10 13:07:19 +0300

    Update PieChart.vue

--- src/views/dashboard/admin/components/PieChart.vue
+++ src/views/dashboard/admin/components/PieChart.vue
@@ -1,5 +1,7 @@
 <template>
-  <div :class="className" :style="{height:height,width:width}" />
+  
+    <div :class="className" :style="{height:height,width:width}" />
+  
 </template>
 
 <script>
@@ -20,18 +22,21 @@ export default {
     },
     height: {
       type: String,
-      default: '300px'
+      default: '550px'
     }
   },
   data() {
     return {
-      chart: null
+      chart: null,
+      acting_for: null
     }
   },
   mounted() {
+    
     this.$nextTick(() => {
       this.initChart()
     })
+    
   },
   beforeDestroy() {
     if (!this.chart) {
@@ -41,39 +46,62 @@ export default {
     this.chart = null
   },
   methods: {
+    
     initChart() {
       this.chart = echarts.init(this.$el, 'macarons')
 
-      this.chart.setOption({
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        legend: {
-          left: 'center',
-          bottom: '10',
-          data: ['Industries', 'Technology', 'Forex', 'Gold', 'Forecasts']
-        },
-        calculable: true,
-        series: [
-          {
-            name: 'WEEKLY WRITE ARTICLES',
-            type: 'pie',
-            roseType: 'radius',
-            radius: [15, 95],
-            center: ['50%', '38%'],
-            data: [
-              { value: 320, name: 'Industries' },
-              { value: 240, name: 'Technology' },
-              { value: 149, name: 'Forex' },
-              { value: 100, name: 'Gold' },
-              { value: 59, name: 'Forecasts' }
-            ],
-            animationEasing: 'cubicInOut',
-            animationDuration: 2600
-          }
-        ]
+      this.acting_for = this.$store.dispatch('user/fetchDash').then(response => {
+
+        this.acting_for = response.acting_for
+        
+        // const gety = this.acting_for
+        // console.log(this.acting_for)
+        // return gety
+        console.log(this.acting_for)
+        // console.log(this.gety)
+        this.chart.setOption({
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          title: {
+              text: 'Acting For',
+              left: 'center'
+          },
+          legend: {
+            left: 'center',
+            bottom: '10',
+            data: ['Defendant', 'Claimant', 'Plaintiff', 'Respondent', 'Applicant', 'Petitioner', 'Appealant', 'Republic', 'Accused', 'Vendor', 'Purchaser', 'Assignor', 'Assignee', 'Chargor', 'Chargee', 'Mortgagor', 'Mortgagee', 'Donor', 'Donee', 'Allotee', 'Agent', 'Principal', 'Seller', 'Buyer', 'Franchisor', 'Franchisee', 'Grantor', 'Creditor', 'offeror', 'Offeree', 'Target Company', 'Acquiring Company']
+          },
+          calculable: true,
+          series: [
+            {
+              name: 'Acting For',
+              type: 'pie',
+              roseType: 'radius',
+              radius: [15, 110],
+              center: ['50%', '20%'],
+              label: {
+                show: false
+                },
+                emphasis: {
+                    label: {
+                        show: true
+                    }
+                },
+              data: this.acting_for,
+              animationEasing: 'cubicInOut',
+              animationDuration: 2600
+            }
+          ]
+        })
+        
       })
+      // this.acting_for = [
+      //         { value: 1, name: 'Defendant' },
+      //         { value: 1, name: 'Claimant' }
+      //       ]
+      
     }
   }
 }
