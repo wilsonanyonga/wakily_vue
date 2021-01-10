d4a8084276c54c12c8f402468af68216ce4fcf56
Wilson Anyonga <wilsonanyonga@gmail.com>
2021-01-10 13:05:12 +0300

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -19,7 +19,8 @@ service.interceptors.request.use(
             // let each request carry token
             // ['X-Token'] is a custom headers key
             // please modify it according to the actual situation
-            config.headers['Authorization'] = 'bearer' + getToken()
+            config.headers['Authorization'] = 'Bearer ' + getToken()
+            // config.headers['Content-Type'] = 'multipart/form-data'
         }
         return config
     },
@@ -64,6 +65,13 @@ service.interceptors.response.use(
                     store.dispatch('user/resetToken').then(() => {
                         location.reload()
                     })
+                })
+            } else if (res.code === 4000){
+                console.log(res.error)
+                Message({
+                    message: res.message || 'Error Updating Data',
+                    type: 'error',
+                    duration: 5 * 1000
                 })
             }
             return Promise.reject(new Error(res.message || 'Error'))
